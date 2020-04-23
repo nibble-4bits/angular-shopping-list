90c62dbaab4ea021fb6b5cd1f87518db4e94fb3a
Luis De Anda <ldeandac16@gmail.com>
2020-04-23 10:38:24 -0500

    Crate item service

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../shared/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  items: Item[];
+
+  constructor() { this.items = []; }
+
+  addItem(item: Item) {
+    this.items.push(item);
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
