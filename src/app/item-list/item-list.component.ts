d7fe2f1f7e7a1901bf6e6e11a1945116c8f7a2d7
Luis De Anda <ldeandac16@gmail.com>
2020-04-23 10:37:54 -0500

    Create item-list component

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../shared/item';
+import { ItemService } from '../services/item.service';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss']
+})
+export class ItemListComponent implements OnInit {
+  items: Item[];
+
+  constructor(private itemSvc: ItemService) { }
+
+  ngOnInit(): void {
+    this.items = this.itemSvc.getItems();
+  }
+
+}
