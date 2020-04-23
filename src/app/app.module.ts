f808671ecce90484858208ed173c225cfc9a4b71
Luis De Anda <ldeandac16@gmail.com>
2020-04-23 10:39:27 -0500

    Add item-input and item-list components to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemInputComponent } from './item-input/item-input.component';
+import { FormsModule } from '@angular/forms';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemInputComponent,
+    ItemListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
